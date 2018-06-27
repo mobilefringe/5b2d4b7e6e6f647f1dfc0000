c06f3c3e83759d7e6977f53044db5063b68a7ac9
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-27 11:49:37 -0400

    Cerritos Towne Center 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -79,7 +79,7 @@
                     if(temp_repo != null) {
                         this.pageBanner = temp_repo[0];
                     } else {
-                        this.pageBanner = "http://via.placeholder.com/1920x400/4f6726/4f6726";
+                        this.pageBanner = "//codecloud.cdn.speedyrails.net/sites/5b2d4b7e6e6f647f1dfc0000/image/jpeg/1529532304000/insidebanner2.jpg";
                     }
                     
                     this.dataLoaded = true;
