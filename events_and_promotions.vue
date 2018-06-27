59ec610ea2918a81786c2df2826c502587ac1544
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-27 12:39:36 -0400

    Cerritos Towne Center 1.0  /events_and_promotions.vue

--- events_and_promotions.vue
+++ events_and_promotions.vue
@@ -111,12 +111,12 @@
             },
             created (){
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Events Banner').images;
-                    if(temp_repo != null) {
-                        this.pageBanner = temp_repo[0];
-                    } else {
-                        this.pageBanner = "//codecloud.cdn.speedyrails.net/sites/5b2d4b7e6e6f647f1dfc0000/image/jpeg/1529532304000/insidebanner2.jpg";
-                    }
+                    // var temp_repo = this.findRepoByName('Events Banner').images;
+                    // if(temp_repo != null) {
+                    //     this.pageBanner = temp_repo[0];
+                    // } else {
+                    //     this.pageBanner = "//codecloud.cdn.speedyrails.net/sites/5b2d4b7e6e6f647f1dfc0000/image/jpeg/1529532304000/insidebanner2.jpg";
+                    // }
                     
                     this.dataLoaded = true;
                 });
