d32f72bdf64c4b9bb3c288c51cf86a548227e9c3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-27 11:20:19 -0400

    Cerritos Towne Center 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -16,7 +16,7 @@
                             <form class="newsletter_form form-horizontal" action="//mobilefringe.createsend.com/t/d/s/vjklyu/" method="post" @submit.prevent="validateBeforeSubmit">
                                 <div class="row">
                                     <div class="col-sm-6" >
-                                        <label for="newsletter_name" class="accessibility">First Name</label>
+                                        <label for="fieldfihudt" class="accessibility">First Name</label>
                                         <input v-model="form_data.first_name" required class="margin_20 form-control" id="fieldfihudt" name="cm-f-fihudt" type="text" placeholder="First Name">
                                     </div>
                                     <div class="col-sm-6" >
@@ -26,20 +26,16 @@
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-12">
-                                        <label for="cm-vjklyu-vjklyu" class="accessibility">Email</label>
+                                        <label for="newsletter_email" class="accessibility">Email</label>
                                         <input v-model="form_data.email" required class="margin_20 form-control" name="cm-vjklyu-vjklyu" type="email" placeholder="Email" id="newsletter_email">
                                     </div>
-                                    <!--<div class="col-sm-6">-->
-                                    <!--    <label for="fieldfihudd" class="accessibility">Phone Number</label>-->
-                                    <!--    <input id="fieldfihudd" v-model="form_data.phone" required class="margin_20 form-control" name="cm-f-fihudd" type="text" placeholder="Phone Number" />-->
-                                    <!--</div>-->
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-12">
                                         <div style="margin-left: 20px">
                                             <label class="checkbox">
                                                 <input name="agree_newsletter" required  type="checkbox">
-                                                    I agree to receive communications from {{ property.name }}.
+                                                I agree to receive communications from {{ property.name }}.
                                             </label>
                                         </div>
             					    </div>
