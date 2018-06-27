b226b2af987450cc0ac8177454cffb0b4c7c8bd0
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-27 13:00:19 -0400

    Cerritos Towne Center 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -84,6 +84,7 @@
 						this.$router.replace({ path: '/promotions' });
 					}
 				// 	this.$breadcrumbs[2].meta.breadcrumb = this.currentPromo.name
+				console.log(this.$breadcrumbs)
 					this.dataLoaded = true;
 				}, error => {
 					console.error("Could not retrieve data from server. Please check internet connection and try again.");
