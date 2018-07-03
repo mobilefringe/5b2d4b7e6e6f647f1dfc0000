2a5a8eb7e16ad4a5aaf138e1ccdc2e7c7a16a9cc
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-07-03 11:53:16 -0400

    Cerritos Towne Center 1.0  /events_and_promotions.vue

--- events_and_promotions.vue
+++ events_and_promotions.vue
@@ -33,7 +33,7 @@
                                 </div>
                                 <div class="col-md-8">
                                     <h4 class="event_name">{{ event.name }}</h4>
-                                    <p class="event_dates"><span v-if="event.tags && event.tags.length >0">{{event.tags[0]}}</span>  | <span v-if="isMultiDay(event)">{{ event.start_date | moment("MMMM D", timezone)}} to {{ event.end_date | moment("MMMM D", timezone)}}</span><span v-else>{{ event.start_date | moment("MMMM D", timezone)}}</span></p>
+                                    <p class="event_dates"><span v-if="event.tags && event.tags.length >0">{{event.tags[0]}} |</span><span v-if="isMultiDay(event)">{{ event.start_date | moment("MMMM D", timezone)}} to {{ event.end_date | moment("MMMM D", timezone)}}</span><span v-else>{{ event.start_date | moment("MMMM D", timezone)}}</span></p>
                                     <div class="event_desc" v-html="event.description_short"></div>
                                     <router-link :to="{ name: 'eventDetails', params: { id: event.slug, banner: pageBanner }}">
                                         <p class="event_link">Event Details <i class="fas fa-angle-double-right"></i></p>
