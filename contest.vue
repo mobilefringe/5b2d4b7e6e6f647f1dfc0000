ee2f5653534e53008419daf6a3cbedfb5fd5e15b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-09 13:12:17 -0400

    Cerritos Towne Center 1.0  /contest.vue

--- contest.vue
+++ contest.vue
@@ -106,6 +106,7 @@
             },
             created() {
                 this.$store.dispatch("getData", "contests").then(response => {
+                    console.log(response)
                     this.currentContest = this.findContestByShowOnSlug('cerritos-contest');
                     console.log(this.currentContest)
                     this.dataLoaded = true;
