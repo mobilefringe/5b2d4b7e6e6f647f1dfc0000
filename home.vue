a7a8b3e7f70300ddde5339ec594e12013ba0cea9
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-20 13:31:21 -0400

    Cerritos Towne Center 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -7,7 +7,7 @@
                     <div class="prev"></div>
                     <slick ref="slick" :options="slickOptions">
                         <div v-if="homeBanners" v-for="banner in homeBanners">
-                            <div v-if="banner.url" class="banner_height">
+                            <div v-if="banner.name && banner.description" class="banner_height">
                                 <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
                                 <div class="banner_content_container">
                                     <div class="banner_content">
@@ -21,7 +21,9 @@
                                 </div>
                             </div>
                             <div v-else>
-                                <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
+                                <a :href="banner.url">
+                                    <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
+                                </a>
                             </div>
                         </div>
                     </slick>
