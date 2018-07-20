0844c09f1d2a7963adc8dc635ce4f9d37b9b408f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-19 21:14:28 -0400

    Cerritos Towne Center 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -5,7 +5,7 @@
             <div v-if="dataLoaded" v-cloak>
                 <div class="inside_page_header">
                     <div class="main_container position_relative">
-                        <h2>Events</h2>
+                        <h2>Events & Promotions</h2>
                     </div>
                 </div>
                 <div class="main_container">
