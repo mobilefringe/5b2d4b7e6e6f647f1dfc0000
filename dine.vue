f9e038e0387cedc06e356bc9fa30daa11bd84f59
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-07-30 14:20:32 -0400

    Cerritos Towne Center 1.1  /dine.vue

--- dine.vue
+++ dine.vue
@@ -150,6 +150,7 @@
                     'storesByCategoryName',
                     'findCategoryById',
                     'findCategoryByName',
+                    'findSubcategoriesByParentID',
                     'findRepoByName'
                 ]),
                 allStores() {
