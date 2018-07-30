39e1023fcf0e64a241a08683f42c64fb5c50d582
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-07-30 14:22:09 -0400

    Cerritos Towne Center 1.1  /dine.vue

--- dine.vue
+++ dine.vue
@@ -181,6 +181,7 @@
                     //     });
                     // });
                     var dining_cat =  _.find(this.processedCategories, function(o) { return o.name == "Dining"});
+                    var subcategories = [];
                     if (dining_cat !== null && dining_cat !== undefined) {
                        subcategories = findSubcategoriesByParentID(dining_cat.id);
                     }
