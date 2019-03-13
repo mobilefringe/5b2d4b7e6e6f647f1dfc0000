fb050c38532e57f58a157decf01f5ab9ce932896
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-13 15:54:14 -0400

    Cerritos Towne Center 1.1  /dine.vue

--- dine.vue
+++ dine.vue
@@ -180,6 +180,7 @@
                     
                     subcategories = _.map(subcategories, 'name').sort();
                     subcategories.unshift('All Cuisine');
+                    console.log(subcategories)
                     return subcategories;
                 },
                 filterByCategory() {
