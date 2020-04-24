fe05ab73a50549d3fce35fb1889b7b5a6d7d04bf
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-04-24 17:33:27 -0400

    Cerritos Towne Center 1.3  /delivery.vue

--- delivery.vue
+++ delivery.vue
@@ -104,6 +104,16 @@
                                                 </div>
                                             </div>
                                             
+                                            <div v-if="!store.delivery_store && store.takeout_store && store.curbside_store">
+                                                <div class="store_tag position_one" >
+                                                    <div class="store_tag_text">Take Out</div>
+                                                </div>
+                                                
+                                                <div class="store_tag position_two" >
+                                                    <div class="store_tag_text">Curbside</div>
+                                                </div>
+                                            </div>
+                                            
                                             
                                             <div v-if="store.delivery_store && store.takeout_store && store.curbside_store">
                                                 <div class="store_tag position_one" >
