5b5b26f695ca912f4a9e494f1dbd75400a3dadec
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-05-05 14:11:23 -0400

    Cerritos Towne Center 1.3  /stores.vue

--- stores.vue
+++ stores.vue
@@ -150,7 +150,7 @@
                                             <!--    || (store.delivery_store && store.takeout_store && store.curbside_store && store.is_closed)-->
                                             <!--    || store.is_closed-->
                                             <!--    ">-->
-                                            <div v-else-if="(store.delivery_store || store.takeout_store || store.curbside_store) && store.is_closed">
+                                            <div v-else-if="(store.delivery_store || store.takeout_store || store.curbside_store) && store.is_closed || store.is_closed">
                                                 <div class="store_tag position_one" >
                                                     <div class="store_tag_text">Closed</div>
                                                 </div>
