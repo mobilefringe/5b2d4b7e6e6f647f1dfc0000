76cd4ef1e391bd708b7bf7a362dd4f4bd88fc281
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-05-09 13:42:30 -0400

    Cerritos Towne Center 1.3  /delivery.vue

--- delivery.vue
+++ delivery.vue
@@ -183,7 +183,7 @@
                     
                                             <div v-if="store.tags">
             								    <div v-for="(tag, index) in store.tags">
-            								        <div v-if="" class="store_tag" >
+            								        <div class="store_tag" >
                                                         <div class="store_tag_text">{{ tag }}</div>
                                                     </div>
                                                     
