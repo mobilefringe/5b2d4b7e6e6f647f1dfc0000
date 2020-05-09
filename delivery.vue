5157b967b0eb021d61f7f6b86aac250afb37f4d3
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-05-09 14:11:39 -0400

    Cerritos Towne Center 1.3  /delivery.vue

--- delivery.vue
+++ delivery.vue
@@ -211,12 +211,7 @@
                         }
                     });
                     
-                    
-                    
                     this.filteredStores = store_list;
-                    
-                    console.log(this.filteredStores)
-                    
                     return store_list
                 },
                 dropDownCats() {
